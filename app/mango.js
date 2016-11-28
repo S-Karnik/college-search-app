11c1d4b2a522a1af8cbc519f3e155c5f2fff2205
Kevin Hu <kevinhuwest@gmail.com>
2016-11-28 08:36:35 -0500

    mango

---
+++ app/mango.js
@@ -0,0 +1,17 @@
+// Connect to the db
+var databaseUrl = "mongodb://reader:macandcheese314@ds147797.mlab.com:47797/cheese"
+var collections = ["colleges"]
+var mongojs = require("mongojs")
+var db = mongojs(databaseUrl, collections);
+
+db.on('connect', function () {
+    console.log("I'm in.")
+})
+
+db.colleges.find({INSTNM: "Boston"}, function(err, schools) {
+
+  console.log(err);
+  schools.forEach( function(school) {
+    console.log(school);
+  } );
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	title = req.app.get('api_key');
 	res.render("index.jade");
-  console.log("IP address: " + req.connection.remoteAddress);
+  	console.log("IP address: " + req.connection.remoteAddress);
+});
+
+router.get('/search', function(req, res, next) {
+	title = req.app.get('api_key');
+	res.render("search.jade");
+  	console.log("IP address: " + req.connection.remoteAddress);
+});
+
+router.post('/search', function(req, res, next) {
+	title = req.body.college;
+	res.render("results.jade");
+  	console.log("IP address: " + req.connection.remoteAddress);
 });
 
 module.exports = router;
